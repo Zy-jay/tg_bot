aa2f9254a0ac2e8703f67f6a90ff38f32941034c
Luka Fastafolmargo <luka.fastafolmargo@gmail.com>
2023-10-16 20:55:46 +0300

    is nan

--- methods/texts_ru.js
+++ methods/texts_ru.js
@@ -121,7 +121,7 @@ ${formated.prelaunchCalls.map((item, i) => {
             const currentROI = await getROI(tokenInfo.address, tokenInfo?.chain == 'ether' ? 1 : 56, e.timestamp);
             await sleep(2000);
             console.log('currentROI: ', currentROI);
-            result.push(`${elementNumber + 1}. <a href="https://t.me/${escapeHtmlEntities(e.channelInnerLink)}/${escapeHtmlEntities(e.message_id)}">${escapeHtmlEntities(e.channelTitle)}</a>: ${(new Date(parseInt(e.timestamp, 10))).toUTCString().split(' ')[4]} | <b>ROI</b> ${currentROI == 0 || currentROI == NaN ? '🍀' : currentROI}x🔹\n`);
+            result.push(`${elementNumber + 1}. <a href="https://t.me/${escapeHtmlEntities(e.channelInnerLink)}/${escapeHtmlEntities(e.message_id)}">${escapeHtmlEntities(e.channelTitle)}</a>: ${(new Date(parseInt(e.timestamp, 10))).toUTCString().split(' ')[4]} | <b>ROI</b> ${currentROI == 0 || isNaN(currentROI) ? '🍀' : currentROI + 'x'}🔹\n`);
 
         }
         console.log(result);
